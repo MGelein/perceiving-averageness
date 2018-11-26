a8c63f731a760fad934cabcca28d2041fe330a8a
Mees Gelein <meesgelein@gmail.com>
2018-11-26 12:44:08 +0100

    Basic element creation works now

---
+++ js/dom.js
@@ -0,0 +1,56 @@
+/**
+ * DOM.JS
+ * In this file some methods reside that can be used to interact with HTML elements.
+ * This is mostly to interact with native elements for input, such as textareas, sliders,
+ * radiobuttons etc.
+ */
+var RESIZE_FACTOR = 0;
+
+ /**
+  * JQuery entry point, this will probably trigger the loading of the external data files.
+  * Also calculates the resize factor.
+  */
+$(document).ready(function(){
+    //Add a resize handler
+    $(window).resize(function(){
+        //Calculate the resize factor
+        RESIZE_FACTOR = window.innerWidth / SKETCH_WIDTH;
+    });
+    //Also actually trigger it a first time
+    $(window).resize();
+});
+
+
+/**
+ * Adds a new element to the document. Use the provided tagname and
+ * tagContent.
+ * @param {String} id   the id we have to give to the entity
+ * @param {String} tagName  the name of the tag. f.e. h1
+ * @param {String} innerHTML the content of the tag. Can be text but also more HTML.
+ * @return the JQuery selection of the element
+ */
+function addElement(id, tagName, innerHTML){
+    //Append the html for the new object
+    $('body').append("<" + tagName + " id='" + id + "'>" + innerHTML + "</" + tagName + ">");
+    //now return the object we added
+    return $('#' + id);
+}
+
+/**
+ * Sets the element with the given id to the provided positions
+ * @param {String} id  the id of the element
+ * @param {Number} x   the x-coord we have to place the element at.
+ * @param {Number} y   the y-coord we have to place the element at.
+ * @return the JQuery object that we modified
+ */
+function setPosition(id, x, y){
+    //Check if the id contains a hash, if so, remove it.
+    id = id.replace(/#/g, '');
+    //Calculate the change factor
+    x *= RESIZE_FACTOR;
+    y *= RESIZE_FACTOR;
+    //Set the position of the element
+    $('#' + id).offset({top: y, left: x});
+    //Return the object we moved
+    return $('#' + id);
+}
--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 /**
+ * MAIN.JS
+ * This file contains the p5.js sketch, or at least the starting methods for it. At a later
+ * stage some of the logic may be turned into its own file. 
+ */
+var SKETCH_WIDTH = 1280;
+var SKETCH_HEIGHT = 720;
+
+/**
  * Entry point of the P5 code, separate from
  * the JQuery entry point
  */
 function setup(){
     //Create a 1280 x 720 pixel canvas. We might need to increase this to improve resolution
     //However this might also mean that we need to lower the framerate.
-    createCanvas(1280, 720);
+    createCanvas(SKETCH_WIDTH, SKETCH_HEIGHT);
     $('canvas').attr('style', '');
     //Set the frameRate quite low, we don't need too high and lower might help to increase the
     //Resolution later on.
     frameRate(60);
+
+    addElement("thingy", "h1", "Testing");
 }
 
 /**
@@ -20,8 +30,9 @@ function draw(){
     background(255, 125, 125);
     noStroke();
 
+    setPosition("thingy", mouseX, mouseY);
+
     //Follow the mouse, and draw a purplish circle
     fill(125, 125, 255);
     ellipse(mouseX, mouseY, 20, 20);
-    console.log(mouseX, mouseY);
 }
