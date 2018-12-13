7664341dd36ff8f3f35af5301d1506f87bf26f40
Mees Gelein <meesgelein@gmail.com>
2018-12-13 16:46:30 +0100

    updated the thingies

--- js/slides.js
+++ js/slides.js
@@ -8,4 +8,7 @@
  */
 function slideOne(m){
     m.add(new Para(30, 120, SKETCH_WIDTH - 200, getS("INTRO")));
+    m.add(new Button(getS("CONTINUE"), 30, 600, function(){
+        console.log("yeah");
+    }));
 }
