1082404f5188159b498539ec3685c4a2a42496be
Mees Gelein <meesgelein@gmail.com>
2018-12-13 14:09:34 +0100

    Added a header to show where we are, now let's make it interactive

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * MAIN.JS
  * This file contains the p5.js sketch, or at least the starting methods for it. At a later
  * stage some of the logic may be turned into its own file. 
- */ 
+ */
 var SKETCH_WIDTH = 1280;
 var SKETCH_HEIGHT = 720;
 
@@ -55,6 +55,8 @@ function setup() {
     setColors();
     //Show the first slide
     nextSlide();
+    //Nice thick lines
+    strokeWeight(2);
 }
 
 /**
@@ -67,31 +69,34 @@ function draw() {
 
     //Update the modals
     updateModals();
+
+    //Now show the header
+    updateHeader();
 }
 
 /**
  * Continue on to the next slide
  */
-function nextSlide(){
+function nextSlide() {
     //Hide all current modals
-    modals.map(function(m){m.hide()});
+    modals.map(function (m) { m.hide() });
     //Increase slidenumber
-    slideNumber ++;
+    slideNumber++;
     //Make a new modal
     let modal = new Modal(getTitle(), getQuestion());
     //Switch slidenumber, to get the correct slide layout
-    switch(slideNumber){
+    switch (slideNumber) {
         case 0:
             slideOne(modal);
-        break;
-        case 1: 
-        break;
+            break;
+        case 1:
+            break;
         case 2:
-        break;
+            break;
         case 3:
-        break;
+            break;
         case 4:
-        break;
+            break;
     }
     //Finally show the modal
     modal.show();
@@ -100,15 +105,64 @@ function nextSlide(){
 /**
  * Gets the appropriate title for the current slide
  */
-function getTitle(){
+function getTitle() {
     return getS("TITLE_" + slideNumber);
 }
 
 /**
  * Returns the question for this slide
  */
-function getQuestion(){
+function getQuestion() {
     return getS("QUEST_" + slideNumber);
+}
+
+/**
+ * Updates and renders the top header
+ */
+function updateHeader() {
+    //Draw the dropshadow
+    fill(0, 80);
+    noStroke();
+    rect(-10, -10, SKETCH_WIDTH + 20, 84);
+    //Draw the rect that holds the header
+    fill(colorD);
+    stroke(colorB);
+    //Draw the rect
+    rect(-10, -10, SKETCH_WIDTH + 20, 80);
+    //Draw the question makrers depending on slideNumber
+    drawSlideMarker(slideNumber);
+}
+
+/**
+ * Draw all the slides, and fill up until the number provided
+ * @param {Number} num 
+ */
+function drawSlideMarker(num) {
+    const startX = 140;
+    const startY = 35;
+    const stepX = 100;
+    const radius = 30;
+    //First draw a line
+    strokeWeight(9);
+    stroke(colorA);
+    line(-10, startY, SKETCH_WIDTH + 20, startY);
+    strokeWeight(3);
+    //For all questions
+    for (let i = 0; i < 11; i++) {
+        if (i < num) {//Questions we've had
+            fill(colorC);
+            stroke(colorA);
+            ellipse(i * stepX + startX, startY, radius, radius);
+        } else if (i > num) {//Question's we haven't reached
+            fill(colorD);
+            stroke(colorA);
+            ellipse(i * stepX + startX, startY, radius, radius);
+        } else {//The question we're at right now
+            fill(colorB);
+            stroke(colorA);
+            ellipse(i * stepX + startX, startY, radius * 1.3, radius * 1.3);
+        }
+    }
 }
 
 /**
